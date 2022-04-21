6966d6de9e8dab7e5a48025faef95210604a2c14
SeaRise <hhssearise@foxmail.com>
2022-04-21 19:44:11 +0800

    y

--- dbms/src/Flash/Coprocessor/DAGQueryBlockInterpreter.cpp
+++ dbms/src/Flash/Coprocessor/DAGQueryBlockInterpreter.cpp
@@ -529,9 +529,6 @@ void DAGQueryBlockInterpreter::handleJoin(const tipb::Join & join, DAGPipeline &
     size_t max_block_size_for_cross_join = settings.max_block_size;
     fiu_do_on(FailPoints::minimum_block_size_for_cross_join, { max_block_size_for_cross_join = 1; });
 
-//    size_t join_build_concurrency = settings.join_concurrent_build ? std::min(max_streams, build_pipeline.streams.size()) : 1;
-    size_t join_build_concurrency = 1;
-
     JoinPtr join_ptr = std::make_shared<Join>(
         probe_key_names,
         build_key_names,
@@ -540,7 +537,7 @@ void DAGQueryBlockInterpreter::handleJoin(const tipb::Join & join, DAGPipeline &
         tiflash_join.kind,
         tiflash_join.strictness,
         log->identifier(),
-        join_build_concurrency,
+        1,
         tiflash_join.join_key_collators,
         probe_filter_column_name,
         build_filter_column_name,

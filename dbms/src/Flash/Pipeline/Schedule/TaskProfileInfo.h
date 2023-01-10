b7c09d232ed74db90b15faee49734e2e84dd161d
SeaRise <hhssearise@foxmail.com>
2023-01-10 10:06:56 +0800

    u

--- dbms/src/Flash/Pipeline/Schedule/TaskProfileInfo.h
+++ dbms/src/Flash/Pipeline/Schedule/TaskProfileInfo.h
@@ -21,80 +21,50 @@
 
 namespace DB
 {
-template <typename UnitType>
-class TaskProfileInfo
-{
-public:
-    UnitType execute_time = 0;
-    UnitType execute_pending_time = 0;
-
-    UnitType spill_time = 0;
-    UnitType spill_pending_time = 0;
-
+#define PROFILE_MEMBER(UnitType)       \
+    UnitType execute_time = 0;         \
+    UnitType execute_pending_time = 0; \
+    UnitType spill_time = 0;           \
+    UnitType spill_pending_time = 0;   \
     UnitType await_time = 0;
 
+class LocalTaskProfileInfo
+{
 public:
-    void startTimer()
-    {
-        stopwatch.start();
-    }
+    PROFILE_MEMBER(UInt64)
 
-    UInt64 elapsedFromPrev()
-    {
-        return stopwatch.elapsedFromLastTime();
-    }
+public:
+    void startTimer();
 
-    void addExecuteTime(UInt64 value)
-    {
-        execute_time += value;
-    }
+    UInt64 elapsedFromPrev();
 
-    void addExecutePendingTime()
-    {
-        execute_pending_time += elapsedFromPrev();
-    }
+    void addExecuteTime(UInt64 value);
 
-    void addSpillTime(UInt64 value)
-    {
-        spill_time += value;
-    }
+    void addExecutePendingTime();
 
-    void addSpillPendingTime()
-    {
-        spill_pending_time += elapsedFromPrev();
-    }
+    void addSpillTime(UInt64 value);
 
-    void addAwaitTime()
-    {
-        await_time += elapsedFromPrev();
-    }
+    void addSpillPendingTime();
 
-    template <typename Other>
-    void merge(const Other & other)
-    {
-        execute_time += other.execute_time;
-        execute_pending_time += other.execute_pending_time;
-        spill_time += other.spill_time;
-        spill_pending_time += other.spill_pending_time;
-        await_time += other.await_time;
-    }
+    void addAwaitTime();
 
-    String toJson() const
-    {
-        return fmt::format(
-            R"({{"execute_time_ns":{},"execute_pending_time_ns":{},"spill_time_ns":{},"spill_pending_time_ns":{},"await_time_ns":{}}})",
-            execute_time,
-            execute_pending_time,
-            spill_time,
-            spill_pending_time,
-            await_time);
-    }
+    String toJson() const;
 
 private:
     Stopwatch stopwatch{CLOCK_MONOTONIC_COARSE};
 };
 
-using LocalTaskProfileInfo = TaskProfileInfo<UInt64>;
-using GlobalTaskProfileInfo = TaskProfileInfo<std::atomic_uint64_t>;
+class GlobalTaskProfileInfo
+{
+public:
+    PROFILE_MEMBER(std::atomic_uint64_t)
+
+public:
+    void merge(const LocalTaskProfileInfo & local_one);
+
+    String toJson() const;
+};
+
+#undef PROFILE_MEMBER
 
 } // namespace DB
